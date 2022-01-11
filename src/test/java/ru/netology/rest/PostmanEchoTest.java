a5afb286d6405ef5d1aa8f112f3834eb47b0d8e0
Валентина Юрова <visible7@mail.ru>
2022-01-11 09:50:41 +0500

    Add postman echo test

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class PostmanEchoTest {
+    @Test
+    void shouldValidResponseFromPostmanEcho() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("application/json")
+                .body("{data:\"some data\"}")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("{data:\"some data\"}"));
+    }
+}
